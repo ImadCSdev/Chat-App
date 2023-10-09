0b0428f0d1afd74004ad248aab2c6110fa47726f
ImadSCdev <imadmoujahid7@gmail.com>
2023-10-10 00:56:12 +0100

    second commit

--- client/src/Routes.jsx
+++ client/src/Routes.jsx
@@ -1,28 +1,25 @@
  
 import { useContext } from "react";
 import {UserContext} from "./UserContext.jsx";
-import Register   from "./Register"
-
+import RegisterAndLoginForm from "./RegisterAndLoginForm.jsx";
+import Chat from "./Chat.jsx";
 
 export default function Routes  () {
 
     const {username, id} = useContext(UserContext);
-    if (username) {
-        return   'logged in';
-          
-        
-                  
 
+    if (username) {
+        return   <Chat/> 
+          
       }
 
 
 
  return (
-   <Register/>
+   <RegisterAndLoginForm/>
  )
 
 }
 
 
-
-//min 50 
+ 
